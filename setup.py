7e7c69294fda0aba25dc3717b85d6e56b146f159
Ayush Jain <ayushjain.9f@gmail.com>
2026-08-30 02:15:49 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,16 +4,18 @@ from setuptools import setup, find_packages
 setup(
     name="yosan",
     version="1.0.0",
-    py_modules=["main", "auth", "server"],
+    py_modules=["yosan", "auth", "server"],
     packages=find_packages(),
     install_requires=[
         "requests",
         "dnspython",
-        "openpyxl"
+        "openpyxl",
+        "reportlab",
+        "libsql-client"
     ],
     entry_points={
         "console_scripts": [
-            "yosan=main:main",
+            "yosan=yosan:main",
         ],
     },
 )
